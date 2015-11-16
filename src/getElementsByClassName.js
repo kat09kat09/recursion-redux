9d5225fb243630c513f3bb12f5de17fa85335911
kat09kat09 <kat09kat09@gmail.com>
2015-11-16 16:40:27 -0500

    Implemented getElementsByClassName. Pair: Adam Dobkin

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+// NEW VERSION! 
+
+var getElementsByClassName = function(className, node) {
+    node = node || document.body;
+    var nodes = [];
+    if ("classList" in node && node.classList.contains(className)) { //saves having to nest "if" statements. If classList isn't found in node, node.classList.contains(className) is never evaluated, preventing a "cannot read property" error which results from trying to read contains on undefined (when node isn't an element).
+        nodes.push(node);
+    }
+    for (var i = 0; i < node.childNodes.length; i++) {
+        nodes = nodes.concat(getElementsByClassName(className, node.childNodes[i]));
+    }
+    console.log('current nodes', nodes);
+    return nodes;
 };
+
